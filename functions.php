a3680fa86192eedae7bbeb349e6ae880be657ebd
Ganes556 <gustiganes@gmail.com>
2022-06-06 10:45:50 +0800

    adding dashboard & sessions

--- functions.php
+++ functions.php
@@ -1,28 +1,31 @@
 <?php    
     // include database 
     include('./config.php');         
-
+    // login
     function login() {
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $conn = conn();      
-            extract($_POST);
+        $conn = conn();      
+        extract($_POST);
 
-            $sql = "SELECT * FROM users WHERE Username= ?  AND Password= ?";
-            $prepare = $conn -> prepare($sql);
-            $prepare -> bind_param("ss", $username, $password);
-            $prepare -> execute();
-            $result = $prepare -> get_result();
-            if ($result->num_rows > 0) {
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    echo "id: " . $row["idUser"] . " - Name: " . $row["Username"]. " " . $row["Password"]. "<br>";
-                }
-            } else {
-                echo "0 results";
-            }
-            $conn->close();        
-        }  
-        
+        $sql = "SELECT * FROM users WHERE Username= ?  AND Password= ?";
+        $prepare = $conn -> prepare($sql);
+        $prepare -> bind_param("ss", $username, $password);
+        $prepare -> execute();
+        $result = $prepare -> get_result();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            // get name from $result
+            $row = $result->fetch_assoc();
+            
+            // set session
+            $_SESSION["id"] = $row["idUser"];
+            $_SESSION["nama"] = $row["Nama"];
+            $_SESSION["username"] = $row["Username"];
+            header("Location: dashboard.php");
+            die();
+        } else {
+            echo "username or password is incorrect!";
+        }
+        $conn->close();        
     }    
     // function register
     function register() {                
@@ -53,5 +56,35 @@
             return;
         }  
         
+    }
+    function checkLogin(){
+        if(isset($_SESSION["id"])){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    function cors() {
+        // Allow from any origin
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+        
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                // may also be using PUT, PATCH, HEAD etc
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        
+            exit(0);
+        }        
     }
 ?>
--- login.php
+++ login.php
@@ -1,4 +1,13 @@
-<?php session_start() ?>
+<?php 
+    ini_set( 'session.cookie_httponly', 1 );
+    session_start();
+    include('functions.php');     
+    cors();
+    if(checkLogin()){
+        header("Location: dashboard.php");
+        die();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,14 +16,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
-<body>    
-    <?php include('functions.php'); ?>    
-    <?php login() ?>   
-    <form action="login.php" method="post">
-        <input type="text" name="username" placeholder="username">
-        <input type="password" name="password" placeholder="password">
-        <input type="submit" value="Login">
-    </form>
-    <a href="register.php">Register</a>
+<body>
+    <?php        
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            login();
+        }else{
+    ?>
+        <form action="login.php" method="post">
+            <input type="text" name="username" placeholder="username">
+            <input type="password" name="password" placeholder="password">
+            <input type="submit" value="Login">
+        </form>
+        <a href="register.php">Register</a>
+    <?php }?>
 </body>
 </html>
